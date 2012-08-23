b48f0a4d7c9290c4b477e6f27bd1efcb6fac647e
Wes John-Alder <wes@punkave.com>
2012-08-23 10:49:12 -0400

    Made the demo more simple

---
+++ demo/demosrc.js
@@ -0,0 +1,106 @@
+var ControlPanel = Backbone.View.extend({
+
+  events: {
+    "change [data-sort-control]":   "sort",
+    "change [data-filter-control]": "filter"
+  },
+
+  sort: function(event) {
+    var val = $(event.target).val();
+
+    if (val) {
+      collectionView.setSortKey(val);
+    }
+
+    return false;
+  },
+  
+  filter: function(event) {
+    var val = $(event.target).val();
+
+    if (val) {
+      collectionView.setFilter(function(item) {
+        return item.get('name') == val;
+      });
+    } else {
+      collectionView.setFilter(function(item) {
+        return true;
+      });
+    }
+
+    return false;
+  }
+
+});
+
+var CreateView = Backbone.View.extend({
+
+  events: {
+    "submit":   "newItem"
+  },
+
+  newItem: function(event) {
+
+    var args = {};
+
+    args.name = this.$('[data-name-input]').val();
+    args.id = this.$('[data-id-input]').val();
+
+    this.collection.add(args);
+
+    this.$('[data-name-input]').val('');
+    this.$('[data-id-input]').val('');
+
+    return false;
+  }
+
+});
+
+var EditView = Backbone.View.extend({
+  events: {
+    "submit":               "saveItem",
+    "click [data-cancel]":  "cancel"
+  },
+
+  setModel: function(item) {
+    this.model = item;
+    this.render();
+  },
+
+  saveItem: function(event) {
+    if (this.model) {
+      this.model.set(this.getArgs());
+    }
+
+    event.preventDefault();
+    return false;
+  },
+
+  cancel: function(event) {
+
+    this.setModel(null);
+
+    event.preventDefault();
+    return false;
+  },
+
+  render: function() {
+    if (this.model) {
+      this.$('[data-name-input]').val(this.model.get('name'));
+      this.$('[data-id-input]').val(this.model.get('id'));
+    } else {
+      this.$('[data-name-input]').val('');
+      this.$('[data-id-input]').val('');
+    }
+
+    return this;
+  },
+
+  getArgs: function() {
+    var args = {};
+    args.name = this.$('[data-name-input]').val();
+    args.id = this.$('[data-id-input]').val();
+
+    return args;
+  }
+});
